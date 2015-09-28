e1890cff13d5f343dfa8be429511f3b4d5ae5499
Freek Boelders <freek@athom.nl>
2015-09-28 18:36:06 +0200

    Appstore ready + extra functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var self = module.exports;
+var request = require('request');
 
 module.exports.init = function(){
     Homey.log ("Wikipedia app started");
@@ -9,22 +10,41 @@ module.exports.init = function(){
     Homey.manager('speech-input').on('speech', self.onSpeech)
 }
 
-module.exports.requestWiki = function ( spoken_text ) {
-  var request = require('request');
-  var extract;
+module.exports.requestWikiSearch = function ( spoken_text ) {
+    request({url:__("api_link") + '?action=query&list=search&format=json&srsearch=' + spoken_text, json:true}, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        if (body.query.searchinfo.totalhits == 0) {
+          self.askOutput(__("no_info") + spoken_text + __("try_again")); //No information about your search query
+        } else {
+          self.requestWikiPage(body.query.search[0].title); //Use the title of the first result to request that page
+        }
+      }
+    })
+}
 
-    request({url:'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&redirects=&titles=' + spoken_text, json:true}, function (error, response, body) {
+module.exports.requestWikiPage = function ( wiki_title ) {;
+    request({url:__("api_link") + '?format=json&action=query&prop=extracts&exintro=&explaintext=&redirects=&titles=' + wiki_title, json:true}, function (error, response, body) {
       if (!error && response.statusCode == 200) {
 
         for(var id in body.query.pages){ //Read the page id number 
             Homey.log("id: " + id); //What is the id?
-            if (id == -1) {
-              //No result found
-              self.speakOutput("Homey coudn't find any information about " + spoken_text);
+            if (id == -1) { //No result found
+              self.askOutput(__("no_info") + wiki_title + __("try_again")); //No information about your search query
             } else {
-              extract = body.query.pages[id].extract; //What is the extract within that id?
+              var extract = body.query.pages[id].extract; //What is the extract within that id?
+              var extract = extract.replace(/ *\([^)]*\) */g, " "); //Remove () for a space
+              var extract = extract.replace(/ *\[[^)]*\] */g, " "); //Remove [] for a space
 
-              self.speakOutput(extract); //Speak result
+              var split = extract.split('.');
+              var output = split[0] + '.' + split[1] + '.' + split[2]; //First three sentences
+
+              if (split[4] != null) {
+                var more = __("more", { "wiki_title": wiki_title })
+                //self.readMore(extract, wiki_title); //Speak result... but there is more
+                self.speakOutput(output); //Speak result
+              } else {
+                self.speakOutput(output); //Speak result
+              }
             }
           }
       }
@@ -35,40 +55,85 @@ module.exports.requestWiki = function ( spoken_text ) {
 module.exports.onSpeech = function(speech) {
     Homey.log("Speech is triggered");
 
-    var spoken_text;
+    var speechInput;
 
     // loop all triggers
     speech.triggers.forEach(function(trigger){
 
         Homey.log ("speech.transcript: " + speech.transcript);
 
-        var replace1 = speech.transcript.replace("wikipedia", ""); //Replace Wolfram (trigger) with nothing
-        var replace2 = replace1.replace("wiki", ""); //Replace wiki (trigger) with nothing
-        var replace3 = replace2.replace("question", ""); //Replace question (trigger) with nothing
-        var replace4 = toTitleCase(replace3);
+        speechInput = speech.transcript; //Replace Wolfram (trigger) with nothing = speech.transcript.replace("wikipedia", ""); //Replace Wolfram (trigger) with nothing
+
+        speechInput = speechInput.replace("wikipedia", ""); //Replace Wolfram (trigger) with nothing
+        speechInput = speechInput.replace("wiki", ""); //Replace wiki (trigger) with nothing
+        speechInput = speechInput.replace("question", ""); //Replace question (trigger) with nothing
+        speechInput = toTitleCase(speechInput);
 
         function toTitleCase(str){
             return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
         } //Make of every word the first letter uppercase
 
-        spoken_text = replace4.replace(/" "/g, "_"); //Replace spaces with _    /" "/g used to replace all the spaces
-                
+        speechInput = speechInput.replace(/" "/g, "_"); //Replace spaces with _    /" "/g used to replace all the spaces
+
+        Homey.log(speechInput);        
     });
 
-    Homey.log ("spoken_text: " + spoken_text);
-    self.requestWiki(spoken_text);
+    Homey.log ("speechInput: " + speechInput);
+    self.requestWikiSearch(speechInput);
 }
 
 module.exports.speakOutput = function( output ){
     Homey.log("speakOutput");
     Homey.log(output);
 
-    //Homey.manager('speech-output').say( __(output );
+    Homey.manager('speech-output').say( (output) );
 }
 
 module.exports.askOutput = function( output ){
     Homey.log("askOutput");
     Homey.log(output);
 
-    //Homey.manager('speech-output').ask( __(output );
+    Homey.manager('speech-input').ask( (output), function( err, result) {
+      if( err ) {
+        Homey.error( err );
+        return;
+      }
+      self.requestWikiSearch(result);
+    });
+}
+
+module.exports.readMore = function( output, question, more ){
+    Homey.log("readMore");
+    Homey.log(output);
+
+    var fullOutput = output;
+    var split = output.split('.');
+    var output = split[0] + '.' + split[1] + '.' + split[2]; //First three sentences
+
+    if (more == true) {
+      var splitOutput = "";
+      for (var i = 3; i < split.length; i++) { //Create output with the rest of the text without first 3 sentences
+        splitOutput = splitOutput + "." + split[i];
+      }
+      self.speakOutput(splitOutput);
+      return;
+    }
+
+    Homey.manager('speech-input').ask( (output + __("more", {"wiki_title": question})), function( err, speech) {
+      if( err ) {
+        Homey.error( err );
+        return;
+      }
+
+      if (speech.indexOf(__("yes")) > -1) {
+        Homey.log ()
+        self.readMore( fullOutput, question, true);
+      } else if (speech.indexOf(__("no")) > -1) {
+        self.speakOutput( __("ok") );
+        return;
+      } else {
+        self.askOutput( __("yes_no") );
+      } 
+
+    });
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,16 @@
 ### Wikipedia app for Homey
+Give your Homey extra knowledge by connecting it to Wikipedia.
 
-This app connects Homey with Wikipedia.
+---
+##### Speech triggers: 
 
-* Supports single results
+You can trigger this app by saying: "Wikipedia" or "Wiki" and then the word you are looking for.
+
+For example:
+"Wikipedia, personal assistant" --> gives you a definition about what a personal assistant (like Homey) is.
+
+---
+##### Features: 
+
+* Ask Wikipedia about any word which is available on Wikipedia
+* Can be used in your own language (English, Dutch)
